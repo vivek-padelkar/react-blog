d7097b5a710b0193155413daf4c85fd3af27b283
vivek.padelkar <vivek.padelkar@gmail.com>
2022-03-26 10:58:34 +0530

    minor cahnges and bug solved for newely addrd profile

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,13 @@ app.use('/api/upload', uploadRoutes)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, `/uploads`)))
 
+if (process.env.NODE_ENV === 'PROD') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
